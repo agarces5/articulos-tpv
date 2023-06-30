c0d0ee5afce14c0556c389f6bbc2ca5d6e086dc4
Antonio Garcés <garces080995@gmail.com>
2023-07-01 01:24:33 +0200

    Refactor: extract button callback and add use_family hook to get list of families

--- src/components/families.rs
+++ src/components/families.rs
@@ -3,43 +3,42 @@ use yew::prelude::*;
 
 use crate::{
     context::filters::{Filter, FilterContext},
+    hooks::use_family::use_family,
     models::familias::Familia,
 };
+
+fn set_filter(filter_ctx: UseReducerHandle<Filter>) -> Callback<MouseEvent> {
+    // Creamos el callback
+    Callback::from(move |e: MouseEvent| {
+        let target = e
+            .target_dyn_into::<HtmlElement>()
+            .unwrap()
+            .get_attribute("value");
+        if let Some(value) = target {
+            filter_ctx.dispatch(Filter::new(
+                filter_ctx.cajtpv.to_string(),
+                value,
+                filter_ctx.panel.to_owned(),
+            ));
+        }
+    })
+}
 
 #[function_component(Families)]
 pub fn families() -> Html {
     // Leemos los filtros
     let filter_context = use_context::<FilterContext>()
         .expect("Se esta intentando acceder al contexto de Filtros fuera del Provider");
-
     // Get families
-    let familias_json = include_str!("../mocks/familias.json");
-    let familias: Vec<Familia> =
-        serde_json::from_str(familias_json).expect("Failed to conver familias.json into model");
-    // Creamos el callback
-    let set_filter = Callback::from(move |e: MouseEvent| {
-        let filter_context = filter_context.clone();
-        let target = e
-            .target_dyn_into::<HtmlElement>()
-            .unwrap()
-            .get_attribute("value");
-        if let Some(value) = target {
-            filter_context.dispatch(Filter::new(
-                filter_context.cajtpv.to_string(),
-                value,
-                filter_context.panel.to_owned(),
-            ));
-        }
-    });
+    let familias: Vec<Familia> = use_family();
     // Render families
     let familias_html = familias
         .iter()
         .map(|familia| {
-            let set_filter = set_filter.clone();
             html! {
                 <button
                     class={"family card"}
-                    onclick={set_filter}
+                    onclick={set_filter(filter_context.clone())}
                     value={familia.familia().to_string()}>
                         {familia.nombre()}
                 </button>
