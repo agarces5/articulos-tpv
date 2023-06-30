34d2bd4b16aea7d81210f073834c5f77de021bd5
Antonio Garcés <garces080995@gmail.com>
2023-07-01 01:47:25 +0200

    Refactor: Extract callback

--- src/components/controls.rs
+++ src/components/controls.rs
@@ -2,31 +2,32 @@ use crate::context::filters::{Filter, FilterContext};
 use web_sys::HtmlSelectElement;
 use yew::prelude::*;
 
+fn handle_change(filter_ctx: UseReducerHandle<Filter>) -> Callback<Event> {
+    Callback::from(move |e: Event| {
+        let input_element = e.target_dyn_into::<HtmlSelectElement>();
+        if let Some(input) = input_element {
+            let filter = Filter::new(
+                input.value(),
+                filter_ctx.familia.to_owned(),
+                filter_ctx.panel.to_owned(),
+            );
+            filter_ctx.dispatch(filter)
+        }
+    })
+}
+
 #[function_component(Controls)]
 pub fn controls() -> Html {
-    let filter_context = use_context::<FilterContext>()
+    let filter_ctx = use_context::<FilterContext>()
         .expect("Se esta intentando acceder al contexto de Filtros fuera del Provider");
 
-    let onchange = {
-        Callback::from(move |e: Event| {
-            let input_element = e.target_dyn_into::<HtmlSelectElement>();
-            if let Some(input) = input_element {
-                let filter = Filter::new(
-                    input.value(),
-                    filter_context.familia.to_owned(),
-                    filter_context.panel.to_owned(),
-                );
-                filter_context.dispatch(filter)
-            }
-        })
-    };
     html! {
         <section class={"controls"}>
             <h2>{"Controles"}</h2>
             <div class={"css-controles"}>
                 <div>
                     <label htmlfor={"input-cajas"}>{"Cajtpv: "}</label>
-                    <select id={"input-cajas"} {onchange} >
+                    <select id={"input-cajas"} onchange={handle_change(filter_ctx)} >
                         <option value={"all"}>{"Todas"}</option>
                         <option value={"0001"}>{"0001"}</option>
                         <option value={"0002"}>{"0002"}</option>
