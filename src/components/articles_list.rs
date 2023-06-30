faaa92c4988332d397b67ddb69a4dd8cb0083b73
Antonio Garcés <garces080995@gmail.com>
2023-07-01 01:41:16 +0200

    Refactor: Put filters in hook

--- src/components/articles_list.rs
+++ src/components/articles_list.rs
@@ -1,25 +1,10 @@
 use yew::prelude::*;
 
-use crate::{
-    components::articulo_card::ArticuloCard,
-    context::filters::{Filter, FilterContext},
-    hooks::use_articles::use_articles,
-    models::articulo::ListArticulo,
-};
+use crate::{components::articulo_card::ArticuloCard, hooks::use_articles::use_articles};
 
 #[function_component(ArticleList)]
 pub fn article_list() -> Html {
-    let articles: ListArticulo = use_articles();
-    // Get filters
-    let filter_context = use_context::<FilterContext>()
-        .expect("Se esta intentando acceder al contexto de Filtros fuera del Provider");
-    let filters = Filter::new(
-        filter_context.cajtpv.to_owned(),
-        filter_context.familia.to_owned(),
-        filter_context.panel.to_owned(),
-    );
-    // Filter articles
-    let articles = articles.filter(filters);
+    let articles = use_articles();
 
     // Render articles
     let articles = articles
--- src/hooks/use_articles.rs
+++ src/hooks/use_articles.rs
@@ -1,12 +1,23 @@
-use crate::models::articulo::ListArticulo;
+use crate::context::filters::{Filter, FilterContext};
+use crate::models::articulo::{Articulo, ListArticulo};
 use crate::models::articulo_dto::{ArticuloDTO, ListArticuloDTO};
 use yew::hook;
 
 #[hook]
-pub fn use_articles() -> ListArticulo {
+pub fn use_articles() -> Vec<Articulo> {
     // Get the articles
     let articles = include_str!("../mocks/articulos_input.json");
     let articles: Vec<ArticuloDTO> =
         serde_json::from_str(articles).expect("Unable to parse articulos.json");
-    ListArticuloDTO::new(articles).into()
+    let articles: ListArticulo = ListArticuloDTO::new(articles).into();
+    // Get filters
+    let filter_context = yew::use_context::<FilterContext>()
+        .expect("Se esta intentando acceder al contexto de Filtros fuera del Provider");
+    let filters = Filter::new(
+        filter_context.cajtpv.to_owned(),
+        filter_context.familia.to_owned(),
+        filter_context.panel.to_owned(),
+    );
+    // Filter articles
+    articles.filter(filters)
 }
